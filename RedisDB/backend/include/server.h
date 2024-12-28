8dc3d7fd839be1df70bef145978d25e9e63d3252
xrbams <bmsakamali@gmail.com>
2024-12-28 17:58:32 +0200

    Redis DB first iteration with Client/Server

---
+++ RedisDB/backend/include/server.h
@@ -0,0 +1,79 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <sys/epoll.h>
+#include <cstring>
+#include <functional>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+#define PORT 1231
+#define SERVER_IP "127.0.0.1"  // Loopback address
+#define MAX_BUFFER_SIZE 64
+#define MAX_EVENTS 64
+#define EPOLL_TIMEOUT 1000 
+
+const size_t k_max_msg = 4096;
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+static void fd_set_nb(int fd) {
+    errno = 0;
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (errno) {
+        die("fcntl error");
+        return;
+    }
+
+    flags |= O_NONBLOCK;
+
+    errno = 0;
+    (void)fcntl(fd, F_SETFL, flags);
+    if (errno) {
+        die("fcntl error");
+    }
+}
+
+enum {
+    STATE_REQ = 0,
+    STATE_RES = 1,
+    STATE_END = 2,  // mark the connection for deletion
+};
+
+struct Conn {
+    int fd = -1;
+    uint32_t state = 0;     // either STATE_REQ or STATE_RES
+    // buffer for reading
+    size_t rbuf_size = 0;
+    uint8_t rbuf[4 + k_max_msg];
+    // buffer for writing
+    size_t wbuf_size = 0;
+    size_t wbuf_sent = 0;
+    uint8_t wbuf[4 + k_max_msg];
+};
+
+static void state_req(Conn *conn);
+static void state_res(Conn *conn);
+
+void *client_main(void *);
+void *server_main(void *);
+
